42fd8ec480f97877590d10dc9152bfaebae588a9
Elliot Evans <key.draw@gmail.com>
2019-05-12 23:59:47 +0200

    add city serendipity js element files

--- assets/scripts/city-serendipity/main.js
+++ assets/scripts/city-serendipity/main.js
@@ -1,1 +1,5 @@
-import {} from './globe.js'
+import {} from './globe.js';
+import {} from './annotated_schedule';
+import {} from './eiffel_lean.js';
+import {} from './dice.js';
+import {} from './suggestion.js';
